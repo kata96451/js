de3d6af2a589874d1a7fe58cfc12998ea6ea0a42
Katrin <filmprof34@gmail.com>
2021-08-09 19:12:27 +0300

    dz 03.08/4

--- js/main.js
+++ js/main.js
@@ -1,8 +1,46 @@
-const ecma = prompt('Каково "официальное" название Javascript?', '');
+function buttonClic(){
+    const officalname = prompt('Каково "официальное" название Javascript?', '');
 
-if (ecma == EcmaScript){
-    alert('Верно!');
+    if (officalname === 'EcmaScript'){
+        alert('Верно!');
+    }
+    else{
+        alert('Не знаете? "EcmaScript"!');
+    }
 }
-else{
-    alert('Не знаете? "EcmaScript"!');
+function buttonClic2() {
+    let number = prompt('Введите любое число', '');
+
+    if (number > 0){
+        alert('1');
+    }
+
+    else if(number < 0){
+        alert('-1');
+    }
+    else if(number == 0){
+        alert('0');
+    }
+}
+function loginClic(){
+   const login = prompt('Кто пришел?', '');
+
+   if (login === 'Admin'){
+       const password = prompt('Пароль?', '');
+       if (password === 'Черный Властелин'){
+           alert('Добро пожаловать!');
+       }
+       else if(password == null){
+           alert('Вход отменен');
+       }
+       else{
+           alert('Пароль неверен');
+       }
+   }
+   else if (login == null){
+       alert('Вход отменен');
+   }
+   else{
+       alert('Я вас не знаю');
+   }
 }

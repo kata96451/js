2e88d2e5ce3f5292146f7f19f6f89c9dc711bc5d
Katrin <filmprof34@gmail.com>
2021-08-14 08:30:01 +0300

    dz 10.08

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-const ecma = prompt('Каково "официальное" название Javascript?', '');
 
-if (ecma == EcmaScript){
-    alert('Верно!');
+function showNumber() {
+    var i=0;
+
+    while (i<3){
+        i++;
+        alert('number' + i + '!')
+        console.log(i)
+    }
 }
-else{
-    alert('Не знаете? "EcmaScript"!');
+
+function writeNumber() {
+   do {
+       var x = prompt('Введите число больше 100', )
+   }while (x <= 100)
 }

27aaefba6474f2f5b14d22d1297bd39280d33011
Katrin <filmprof34@gmail.com>
2021-08-14 15:35:54 +0300

    dz 10.08/3

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@ function showNumber() {
     var i=0;
 
     while (i<3){
-        alert('number' + i + '!');
+        alert('number' + i + '!')
         i++;
         console.log(i);
     }
 }
 
 function writeNumber() {
-   do {
-       var x = prompt('Введите число больше 100', )
-   }while (x <= 100)
+    do {
+        let x = prompt('Введите число больше 100',);
+    } while (x <= 100  );
+    if (x = null) {break};
 }
